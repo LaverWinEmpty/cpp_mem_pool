1f5a4d2fd2f8b6255fca9b761d511ab76c9b6f3d
KimSeungBin <77220945+LaverWinEmpty@users.noreply.github.com>
2026-02-25 22:18:38 +0900

    chore

--- wip/mem/pool.hpp
+++ wip/mem/pool.hpp
@@ -1,7 +1,7 @@
 #include "allocator.hpp"
 
 //! @brief object pool
-template<typename T, size_t ALIGNMENT>
+template<typename T, size_t ALIGNMENT = sizeof(T)>
 class Pool : public Allocator<global::bit_align(sizeof(T), ALIGNMENT)> {
 public:
     static constexpr size_t BLOCK = global::bit_align(sizeof(T), ALIGNMENT); // same as parent
