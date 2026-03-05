e56246e8160d18fec85d4f0209a138815de08460
KimSeungBin <77220945+LaverWinEmpty@users.noreply.github.com>
2026-03-05 17:56:27 +0900

    Update bitmask.hpp

--- bitmask.hpp
+++ bitmask.hpp
@@ -1,131 +1,130 @@
-// WIP
+#include "../global/bit.hpp"
+#include "cstring"
 
-#include "cstdint"
+using size_t = uintptr_t;
 
 //! @brief static mix-in
 class Bitmask {
 protected:
-    using Word = uintptr_t;
-
-protected:
-    static constexpr size_t BITS  = sizeof(Word) * 8;
-    static constexpr size_t MASK  = BITS - 1;
-    static constexpr size_t SHIFT = global::bit_log2(BITS);
+    template<typename T> struct Operator {
+        static constexpr size_t BITS  = sizeof(T) * 8;
+        static constexpr size_t MASK  = BITS - 1;
+        static constexpr size_t SHIFT = global::bit_log2(BITS);
+    };
 
 public:
-    static void   on(Word flags[], size_t size, size_t index);
-    static void   off(Word flags[], size_t size, size_t index);
-    static void   toggle(Word flags[], size_t size, size_t index);
-    static bool   check(Word flags[], size_t size, size_t index);
-    static void   reset(Word flags[], size_t size);
-    static size_t ffz(const Word flags[], size_t size);
-    static size_t ffs(const Word flags[], size_t size);
+    template<typename T> static void set   (T* flags, size_t index);
+    template<typename T> static void unset (T* flags, size_t index);
+    template<typename T> static void flip  (T* flags, size_t index);
+    template<typename T> static bool check (const T* flags, size_t index);
+    
+public:
+    template<typename T> static size_t ffz(const T* flags, size_t bits);
+    template<typename T> static size_t ffs(const T* flags, size_t bits);
 
 protected:
-    static constexpr size_t count(size_t bits);
+    template<typename T> static constexpr size_t words(size_t bits);
+    template<typename T> static constexpr size_t bytes(size_t bits);
 
 private:
-    static constexpr Word& word(Word flags[], size_t index) { return flags[size_t(index >> SHIFT)]; }
-    static constexpr Word  bit(size_t index) { return Word(1) << size_t(index & MASK); }
+    template<typename T> static constexpr T& word(T* flags, size_t index);
+    template<typename T> static constexpr T  bit(size_t index);
+    template<typename T, bool> static size_t first(const T* flags, size_t size);
 
 protected:
     Bitmask() = default;
 };
 
-void Bitmask::on(Word flags[], size_t size, size_t index) {
-    word(flags, index) |= bit(index);
+template<typename T> constexpr T& Bitmask::word(T* flags, size_t index) {
+    return flags[size_t(index >> Operator<T>::SHIFT)];
 }
 
-void Bitmask::off(Word flags[], size_t size, size_t index) {
-    word(flags, index) &= ~bit(index);
+template<typename T> constexpr T Bitmask::bit(size_t index) {
+    return T(1) << size_t(index & Operator<T>::MASK);
 }
 
-void Bitmask::toggle(Word flags[], size_t size, size_t index) {
-    word(flags, index) ^= bit(index);
+template<typename T> void Bitmask::set(T* flags, size_t index) {
+    word(flags, index) |= bit<T>(index);
 }
 
-bool Bitmask::check(Word flags[], size_t size, size_t index) {
-    return word(flags, index) & bit(index);
+template<typename T> void Bitmask::unset(T* flags, size_t index) {
+    word(flags, index) &= ~bit<T>(index);
 }
 
-size_t Bitmask::ffz(const Word flags[], size_t size) {
-    size_t bytes = count(size);
+template<typename T> void Bitmask::flip(T* flags, size_t index) {
+    word(flags, index) ^= bit<T>(index);
+}
 
-    size_t out = 0;
-    for(size_t i = 0; i < bytes; ++i) {
-        // campare with 0xFF...FF
-        if(flags[i] != Word(-1)) {
-            int cnt = global::bit_ctz(Word(~flags[i]));   // find first zero (unset)
-            out     = size_t((i << SHIFT) + size_t(cnt)); // i * 64 + found index
-            break;
+template<typename T> bool Bitmask::check(const T* flags, size_t index) {
+    return word(flags, index) & bit<T>(index);
+}
+
+template<typename T> size_t Bitmask::ffz(const T* flags, size_t bits) {
+    return first<T, 0>(flags, bits);
+}
+
+template<typename T> size_t Bitmask::ffs(const T* flags, size_t bits) {
+    return first<T, 1>(flags, bits);
+}
+
+template<typename T> constexpr size_t Bitmask::words(size_t n) {
+    return (n + Operator<T>::MASK) / Operator<T>::BITS; // same as Aligner::count
+}
+
+template<typename T> constexpr size_t Bitmask::bytes(size_t n) {
+    return words<T>(n) * sizeof(T); // to byte
+}
+
+template<typename T, bool SET> size_t Bitmask::first(const T* flags, size_t bits) {
+    T target;
+    
+    size_t loop = words<T>(bits);
+    for (size_t i = 0; i < loop; ++i) {
+        if constexpr (SET) {
+            target = flags[i];
+        }
+        else target = ~flags[i];
+        if (target != 0) {
+            int cnt = global::bit_ctz(target);
+            size_t out = (i << Operator<T>::SHIFT) + size_t(cnt);
+            return (out < bits) ? out : T(-1);
         }
     }
-
-    if(out < size) {
-        return out;
-    }
-    return Word(-1); // error
+    return size_t(-1);
 }
 
-size_t Bitmask::ffs(const Word flags[], size_t size) {
-    size_t bytes = count(size);
-
-    size_t out = Word(-1);
-    for(size_t i = 0; i < bytes; ++i) {
-        // campare with
-        if(flags[i] != Word(0)) {
-            int cnt = global::bit_ctz(Word(flags[i]));    // find first set (1)
-            out     = size_t((i << SHIFT) + size_t(cnt)); // i * 64 + found index
-            break;
-        }
-    }
-
-    if(out < size) {
-        return out;
-    }
-    return Word(-1); // error
-}
-
-void Bitmask::reset(Word flags[], size_t size) {
-    std::memset(flags, 0, count(size));
-}
-
-constexpr size_t Bitmask::count(size_t n) {
-    return (n + MASK) / BITS; // same as Aligner::count
-}
-
-template<size_t N, bool = false> class Flags;
-
-template<size_t N> class Flags<N, true>: protected Bitmask {
-public:
-    inline void   on(size_t index) { Bitmask::on(flags, index, N << SHIFT); }
-    inline void   off(size_t index) { Bitmask::off(flags, index, N << SHIFT); }
-    inline void   toggle(size_t index) { Bitmask::toggle(flags, index, N << SHIFT); }
-    inline bool   check(size_t index) { return Bitmask::check(flags, index, N << SHIFT); }
-    inline size_t next() { return Bitmask::ffz(flags, N << SHIFT); }
-    inline void   reset() { Bitmask::reset(flags, sizeof(Word) * N); }
-
-public:
-    Word read(size_t index) const { return flags[index]; }
-
-private:
-    Word flags[N] = { 0 };
-};
-
-template<size_t N> class Flags<N, false>: public Flags<Bitmask::count(N), true> {
-public:
-    static constexpr size_t SIZE = N;
-};
-
-template<bool BASE> class Flags<0, BASE>: protected Bitmask {
-public:
-    static constexpr size_t SIZE = 0;
-
-    inline void   on(size_t index) { }
-    inline void   off(size_t index) { }
-    inline void   toggle(size_t index) { }
-    inline bool   check(size_t index) { return false; }
-    inline size_t next() { return Word(-1); }
-    inline void   reset() { }
-    inline Word   word() { return 0; }
-};
+//template<size_t N, bool = false> class Flags;
+//
+//template<size_t N> class Flags<N, true>: protected Bitmask {
+//public:
+//    inline void on(size_t index) { Bitmask::on(flags, index, N << SHIFT); }
+//    inline void off(size_t index) { Bitmask::off(flags, index, N << SHIFT); }
+//    inline void toggle(size_t index) { Bitmask::toggle(flags, index, N << SHIFT); }
+//    inline bool check(size_t index) { return Bitmask::check(flags, index, N << SHIFT); }
+//    inline size_t next() { return Bitmask::ffz(flags, N << SHIFT); }
+//    inline void reset() { Bitmask::reset(flags, sizeof(Word) * N); }
+//
+//public:
+//    Word read(size_t index) const { return flags[index]; }
+//
+//private:
+//    Word flags[N] = { 0 };
+//};
+//
+//template<size_t N> class Flags<N, false>: public Flags<Bitmask::words(N), true> {
+//public:
+//    static constexpr size_t SIZE = N;
+//};
+//
+//template<bool BASE> class Flags<0, BASE>: protected Bitmask {
+//public:
+//    static constexpr size_t SIZE = 0;
+//
+//    inline void on(size_t index) { }
+//    inline void off(size_t index) { }
+//    inline void toggle(size_t index) { }
+//    inline bool check(size_t index) { return false; }
+//    inline size_t next() { return Word(-1); }
+//    inline void reset() { }
+//    inline Word word() { return 0; }
+//};
